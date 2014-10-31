2d737a5ead397ec1fc0e1d171ca3c75ffd8f2238
unghoanglove <nguyenquochung1989vt@gmail.com>
2014-10-31 07:31:37 +0200

    Add task comment
    I try adding comment module

---
+++ KanProject/comment.ascx
@@ -0,0 +1,11 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="comment.ascx.cs" Inherits="KanProject.WebUserControl1" %>
+<asp:panel ID="panel1" runat="server">
+    <h1>Comment</h1>
+    <asp:TextBox ID="comment" runat="server" Width="400" Height="200"></asp:TextBox>
+    <table>
+        <tr>
+            <td><asp:Button ID="submit" runat="server" Text="Submit" OnClick="submit_Click" /></td>
+            <td><asp:Button ID="cancel" runat="server" Text="Cancel" OnClick="cancel_Click" /></td>
+        </tr>
+    </table>
+</asp:panel>
---
+++ KanProject/comment.ascx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+
+namespace KanProject
+{
+    public partial class WebUserControl1 : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            string projectPath = @"|DataDirectory|\AccessDB.mdb;";
+            string conStr = "Provider = Microsoft.Jet.OLEDB.4.0;" + "Data Source = " + projectPath;
+
+            OleDbConnection Connection = new OleDbConnection();
+            Connection.ConnectionString = conStr;
+            Connection.Open();
+
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = Connection;
+            cmd.CommandText = "INSERT INTO Comments(CommentText)" +"VALUES('"+comment.Text+"')";
+            cmd.CommandType = CommandType.Text;
+
+            Connection.Open();
+            cmd.ExecuteNonQuery();
+            Connection.Close();
+        }
+
+        protected void cancel_Click(object sender, EventArgs e)
+        {
+            comment.Text = string.Empty;
+        }
+    }
+}
